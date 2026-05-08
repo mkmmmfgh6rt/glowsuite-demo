fbe315f2347a02043f1e4ca03709d4ce0dbd0867
Markus <mak263416@gmail.com>
2026-05-08 18:25:34 +0200

    fix services api

--- aura-frontend/api/services.js
+++ aura-frontend/api/services.js
@@ -1,8 +1,20 @@
-import beautyData from "../public/data/beauty_lounge.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
 
 export default function handler(req, res) {
 
   try {
+
+    const filePath = path.join(
+      process.cwd(),
+      "public",
+      "data",
+      "beauty_lounge.json"
+    );
+
+    const jsonData = fs.readFileSync(filePath, "utf8");
+
+    const beautyData = JSON.parse(jsonData);
 
     return res.status(200).json({
       success: true,
