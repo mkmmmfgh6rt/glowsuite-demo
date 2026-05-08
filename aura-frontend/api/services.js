ee33e020143873d9bd73675775315fe1f0a2b4fe
Markus <mak263416@gmail.com>
2026-05-08 18:29:24 +0200

    fix json path

--- aura-frontend/api/services.js
+++ aura-frontend/api/services.js
@@ -7,6 +7,7 @@ export default function handler(req, res) {
 
     const filePath = path.join(
       process.cwd(),
+      "aura-frontend",
       "public",
       "data",
       "beauty_lounge.json"
