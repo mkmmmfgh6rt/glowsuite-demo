10f97ab97a1e74ffff8bd91a5ebccfef11f561fd
Markus <mak263416@gmail.com>
2026-05-08 18:43:11 +0200

    fix absolute json path

--- aura-frontend/api/services.js
+++ aura-frontend/api/services.js
@@ -1,13 +1,17 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default function handler(req, res) {
 
   try {
 
     const filePath = path.join(
-      process.cwd(),
-      "aura-frontend",
+      __dirname,
+      "..",
       "public",
       "data",
       "beauty_lounge.json"
