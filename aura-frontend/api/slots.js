4775b3c5b11c0d8cf906d2f95ea5c40008e5f179
Markus <mak263416@gmail.com>
2026-05-09 15:31:46 +0200

    fix vercel cwd path

--- aura-frontend/api/slots.js
+++ aura-frontend/api/slots.js
@@ -54,7 +54,6 @@ export default async function handler(req, res) {
 
     const filePath = path.join(
       process.cwd(),
-      "aura-frontend",
       "public",
       "data",
       "beauty_lounge.json"
