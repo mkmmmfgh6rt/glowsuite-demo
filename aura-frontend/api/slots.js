b3a24abbd3cbfbd4266b304b0b780d50ea47a3a8
Markus <mak263416@gmail.com>
2026-05-09 14:35:14 +0200

    debug vercel cwd path

--- aura-frontend/api/slots.js
+++ aura-frontend/api/slots.js
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 
+console.log("📂 CWD:", process.cwd());
+
 import {
   calculateSlotsForEmployee
 } from "../frontend-core/availabilityEngine.js";
@@ -52,7 +54,6 @@ export default async function handler(req, res) {
 
     const filePath = path.join(
       process.cwd(),
-      "aura-frontend",
       "public",
       "data",
       "beauty_lounge.json"
